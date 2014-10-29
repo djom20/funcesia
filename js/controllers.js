a9be60fd3e71a72614907556c0016f6cdcd7ad8c
Ing. Jonathan Olier <djom202@gmail.com>
2014-10-29 17:00:40 -0500

    Init Repository

---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+var controllers = angular.module('controllers', []);
+
+controllers.controller('IndexCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $modal, $location) {
+        // functions
+    }]
+);
